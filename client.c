db9aef02c7005cbcd34d22f1ecb7bdbbba376c66
De <ada-977@tlen.pl>
2018-12-14 12:02:09 +0100

    Merge message initialization

--- client.c
+++ client.c
@@ -18,11 +18,7 @@ int main(int argv, char **args)
 {   
     key_t key = 2137 + atoi(args[1]);
     int mq = mq_open(key);
-    message msg;
-    msg.add_info = 1;
-    msg.enemy_units = -1;
-    msg.winner = -1;
-    msg.your_units = -1;
+    message msg = {-1, -1, -1, 1};
     mq_send(mq, &msg, 1);
     
     sleep(30);
