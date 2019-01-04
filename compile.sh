1f8fc7e462e868049cf332b7cf93e6619e1cfb6f
De <ada-977@tlen.pl>
2019-01-04 14:22:29 +0100

    Add compile script

---
+++ compile.sh
@@ -0,0 +1,14 @@
+# -----
+# To run the program after compilation, type
+#./server
+# and in three different terminal windows:
+#./client 0
+#./client 1
+#./client 2
+# -----
+
+clear
+gcc -Wall server.c -o server
+printf "Server compiled\n"
+gcc -Wall client.c -o client -lncurses
+printf "Client compiled\n"
